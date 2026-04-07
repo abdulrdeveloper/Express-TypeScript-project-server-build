da74f911805b8960008beb1a3f8f86b2bb3e9b24
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-04-07 21:01:24 +0500

    add environment variable handling with zod validation and update server port logic

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+//Agar hum npm i zod install karein, to hume uska type package (@types/zod) bhi install karna hota hai taake TypeScript mein errors na aayein. Isi tarah, jis package ke types available hon, unhein bhi install karna chahiye.
+// like ham zod ke andar apnay environment variables ko bi fully typed and validated rakh saktay hain 
+
+import { z } from 'zod';
+
+const envSchema = z.object({
+    PORT: z.string().default('8080'),
+});
+
+function createEnv(env: NodeJS.ProcessEnv) {
+    const safeParseResult = envSchema.safeParse(env);
+    
+    if (!safeParseResult.success) throw new Error(safeParseResult.error.message);
+    return safeParseResult.data;
+
+}
+
+export const env = createEnv(process.env);
